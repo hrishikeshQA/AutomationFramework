11aa2bc1106261e42d5762e2c1725106c68859b9
hrishikeshQA <47062785+hrishikeshQA@users.noreply.github.com>
2019-02-09 23:47:29 +0530

    Update TestBase.py

--- TestBase.py
+++ TestBase.py
@@ -1,21 +1,16 @@
 import xlrd
+from selenium import webdriver
+def readexcel(wb):
+    for sheet in wb.sheets():
+        for i in range(sheet.ncols):
+            if sheet.cell_value(0, i) == 'Automation Key':
+                autoIDcol = i
+            if sheet.cell_value(0, i) == 'OFC_NB_03':
+                for j in range(1, sheet.nrows):
+                    data[sheet.cell_value(j, autoIDcol)] = sheet.cell_value(j, i)
+                break
+    print(data)
 loc = 'C:\\Users\\Wolverine\\PycharmProjects\\Automation\\dataFiles\\OFC_NB.xlsx'
 wb = xlrd.open_workbook(loc)
 data = {}
-innerDataMap = {}
-innerSheets = []
-outData = []
-for sheet in wb.sheets():
-    for i in range(sheet.ncols):
-        if sheet.cell_value(0, i) == 'Automation Key':
-            autoIDcol = i
-        if sheet.cell_value(0, i) == 'OFC_NB_03':
-            for j in range(1, sheet.nrows):
-                if ";" in str(sheet.cell_value(j, i)):
-                    outData.append(sheet.cell_value(j, i))
-                    innerSheets.append(((sheet.cell_value(j, i)).split(';')[0]).split('_')[0])
-                data[sheet.cell_value(j, autoIDcol)] = sheet.cell_value(j, i)
-print(data.get('NB_Status'))
-print(innerSheets)
-print(outData)
-
+readexcel(wb)
